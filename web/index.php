c415d494d30f0ddacafb17ec442ba903eb9f95eb
Sathwik Nadella <sathwik.nadella@gmail.com>
2019-03-06 22:32:01 +0530

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 if (is_uploaded_file($_FILES['bill']['tmp_name'])) {
-$uploads_dir = './files/';
+$uploads_dir = './';
                         $tmp_name = $_FILES['bill']['tmp_name'];
                         $pic_name = $_FILES['bill']['name'];
                         move_uploaded_file($tmp_name, $uploads_dir.$pic_name);
